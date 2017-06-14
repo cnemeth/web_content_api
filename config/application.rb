7cf8bb5094b91aeb62016ca4f616976c16cbb4f7
Csaba Nemeth <csaba.nemeth@csabanemeth.org>
2017-06-13 21:41:56 -0700

    Install RSpec and git some files

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,11 @@ module WebContentApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.test_framework  :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.integration_tool :rspec, :fixture => true, :views => true
+    end
   end
 end
